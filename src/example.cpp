602fe324b439e81c0db972887fec8af3ce497a5c
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-04-08 22:14:22 -0400

    xtensor-python example

--- src/example.cpp
+++ src/example.cpp
@@ -1,21 +1,20 @@
-#include <iostream>
-#include "xtensor/xarray.hpp"
-#include "xtensor/xio.hpp"
-#include "xtensor/xview.hpp"
+#include <numeric>                        // Standard library import for std::accumulate
+#include "pybind11/pybind11.h"            // Pybind11 import to define Python bindings
+#include "xtensor/xmath.hpp"              // xtensor import for the C++ universal functions
+#define FORCE_IMPORT_ARRAY                // numpy C api loading
+#include "xtensor-python/pyarray.hpp"     // Numpy bindings
 
-int main(int argc, char* argv[])
+double sum_of_sines(xt::pyarray<double>& m)
 {
-    xt::xarray<double> arr1
-      {{1.0, 2.0, 3.0},
-       {2.0, 5.0, 7.0},
-       {2.0, 5.0, 7.0}};
-
-    xt::xarray<double> arr2
-      {5.0, 6.0, 7.0};
-
-    xt::xarray<double> res = xt::view(arr1, 1) + arr2;
-
-    std::cout << res;
-
-    return 0;
+    auto sines = xt::sin(m);  // sines does not actually hold values.
+    return std::accumulate(sines.cbegin(), sines.cend(), 0.0);
 }
+
+PYBIND11_MODULE(xtensor_python_test, m)
+{
+    xt::import_numpy();
+    m.doc() = "Test module for xtensor python bindings";
+
+    m.def("sum_of_sines", sum_of_sines, "Sum the sines of the input values");
+}
+
